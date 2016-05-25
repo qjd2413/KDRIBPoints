f58284fea39b12351e94122f1e6df9ba8905a980
Quinn Davis <qjd2413@g.rit.edu>
2016-05-25 00:07:43 -0400

    Initial server setup, creating jshintrc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# KDRIBPoints
+# KDRIB Points
 Points site for the Iota Beta chapter of Kappa Delta Rho
+
+## Setup
+1. Download [node](https://nodejs.org/en/)
+2. Clone repository
+3. Run `npm install`
+4. `node index.js` starts the server
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+var config = require('./config/config');
+var glob = require('glob');
+var bodyParser = require('body-parser');
+var compress = require('compression');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(compress());
+app.use(express.static(config.root + '/public'));
+
+var routers = glob.sync(config.root + '/app/routers/*.js');
+routers.forEach(function (router) {
+    require(router)(app);
+});
+
+app.listen(config.port);
+console.log('Server listening on port', config.port);
