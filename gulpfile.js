09e6e6327c6e89c055b2fd6b85eb80e38dd947c8
Quinn Davis <qjd2413@g.rit.edu>
2016-05-27 01:20:40 -0400

    Implemented CSS injection and jshint in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,50 @@
 
 (function() {
-  'use strict';
+    'use strict';
 
-  var gulp = require('gulp'),
-      nodemon = require('gulp-nodemon'),
-      mainBowerFiles = require('main-bower-files'),
-      inject = require('gulp-inject');
+    var gulp = require('gulp');
+    var nodemon = require('gulp-nodemon');
+    var mainBowerFiles = require('main-bower-files');
+    var inject = require('gulp-inject');
+    var jshint = require('gulp-jshint');
+    var stylish = require('jshint-stylish');
 
-  var jsFiles = mainBowerFiles('**/*.js');
-  var appFiles = './public/app/**/*.js';
-  jsFiles.push(appFiles);
+    //PATHS
+    var jsFiles = mainBowerFiles('**/*.js');
+    var appFiles = './public/app/**/*.js';
+    jsFiles.push(appFiles);
+    var cssFiles = './public/css/**/*.css';
 
-  gulp.task('inject', function() {
-    return gulp.src('public/index.html')
-      .pipe(inject(gulp.src(jsFiles),
-        {relative: true}))
-      .pipe(gulp.dest('./public/'));
-  });
-
-  // automatically restart the server if js files change
-  gulp.task('start', function () {
-    nodemon({
-      script: 'index.js',
-      ext: 'js'
-    }).on('restart', function () {
-      setTimeout(function () {
-
-      }, 500);
+    //inject script and link tags
+    gulp.task('inject', ['hint'], function() {
+        return gulp.src('public/index.html')
+          .pipe(inject(gulp.src(jsFiles),   //inject angular and bower js files
+              {relative: true}))            //only read filenames, not content
+          .pipe(inject(gulp.src(cssFiles),
+              {relative: true}))
+          .pipe(gulp.dest('./public/'));
     });
-  });
 
-  gulp.task('default', [
-      'start',
-      'inject'
-  ]);
+    //run app files through jshint
+    gulp.task('hint', function() {
+        return gulp.src(appFiles)
+            .pipe(jshint())
+            .pipe(jshint.reporter(stylish));
+    });
+
+    // automatically restart the server if js files change
+    gulp.task('start', function () {
+        nodemon({                           //start server
+            script: 'index.js',
+            ext: 'js'
+        }).on('restart', function () {      //wait .5s on restart
+            setTimeout(function () {}, 500);
+        });
+    });
+
+    gulp.task('default', [
+        'start',
+        'inject'
+    ]);
 
 })();
