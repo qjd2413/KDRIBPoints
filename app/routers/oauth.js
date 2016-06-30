ee012b7ed732554b60d086c78b94a4b25a372bfd
Quinn Davis <qjd2413@g.rit.edu>
2016-06-30 15:10:42 -0400

    user's session information is saved

--- app/routers/oauth.js
+++ app/routers/oauth.js
@@ -15,24 +15,37 @@
             callbackURL: config.callback
         },
         function(accessToken, refreshToken, profile, cb) {
-            return cb();
+            console.log(profile.displayName + ' has signed in.');
+            return cb(null, profile);
         })
     );
+
+    passport.serializeUser(function(user, cb) {
+        cb(null, user);
+    });
+
+    passport.deserializeUser(function(obj, cb) {
+        cb(null, obj);
+    });
 
     module.exports = function(app) {
         app.use('/oauth', router);
 
         router.get('/google_signin',
-            passport.authenticate('google', { scope: ['profile'], hd: 'kdrib.org' })
+          passport.authenticate('google', 
+            { scope: ['profile'], hd: 'kdrib.org' })
         );
 
         router.get('/google_callback',
-            passport.authenticate('google', { failureRedirect: '/' }),
-            function(req, res) {
-                // Successful authentication, redirect home.
-                res.redirect('/');
-            }
+          passport.authenticate('google', { failureRedirect: '/error' }),
+          function(req, res) {
+            res.redirect('/');
+          }
         );
+
+        router.get('/user_test', function(req, res) {
+          res.send(req.user);
+        });
 
     };
 
