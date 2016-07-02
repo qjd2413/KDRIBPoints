8d085e9a79d7b9f4321794fd6a4a1a1eeae7b409
Quinn Davis <qjd2413@g.rit.edu>
2016-07-02 01:47:07 -0400

    html5 mode sucks

--- public/app/header.js
+++ public/app/header.js
@@ -5,16 +5,8 @@ angular.module('KDRPoints')
     .then(function(info) {
       $scope.statusButton = {};
       if(info && info.data && info.data.id) {
-        $scope.statusButton.text = 'Sign Out';
-        $scope.statusButton.signin = false;
-
         info = info.data;
         $scope.user = info.firstName.charAt(0) + '. ' + info.lastName;
-        $scope.statusButton.url = '/user/sign_out';
-      } else {
-        $scope.statusButton.text = 'Sign In';
-        $scope.statusButton.signin = true;
-        $scope.statusButton.url = '/user/sign_in';
       }
     }, function(err) {
       console.log('err');
