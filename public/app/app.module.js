3e959fd34a8dac6f30a30073ab6940fb52050f18
Quinn Davis <qjd2413@g.rit.edu>
2016-07-06 22:56:48 -0400

    Refactor states and implement authorization

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -2,18 +2,44 @@
 (function() {
     'use strict';
 
+    var rootCtrl = function($scope, $state, userService) {
+      $scope.$on('$stateChangeStart', function(event, toState) {
+        userService.authenticate()
+          .then(function(auth_status) {
+            console.log(auth_status, toState);
+            if(auth_status.incomplete) {
+              $state.go('root.incomplete');
+            }
+            if(toState.name === 'root.incomplete' && !auth_status.incomplete) {
+              $state.go('root.home');
+            }
+          });
+      });
+    }
+
     var KDRPoints = function($stateProvider, $urlRouterProvider) {
 
         $stateProvider
-        .state('home', {
+        .state('root', {
+            url: '',
+            abstract: true,
+            controller: ['$scope', '$state', 'userService', rootCtrl],
+            template: '<ui-view />',
+            resolve: {
+              user: ['userService', function(userService) {
+                  return userService.getUser();
+              }]
+            }
+        })
+          .state('root.home', {
             url: '/',
             templateUrl: 'app/home/home.html'
-        })
-        .state('incomplete', {
-            url: '/incomplete',
-            templateUrl: 'app/incomplete/incomplete.html',
-            controller: 'incompleteCtrl',
-        });
+          })
+          .state('root.incomplete', {
+              url: '/incomplete',
+              templateUrl: 'app/incomplete/incomplete.html',
+              controller: 'incompleteCtrl',
+          });
 
         $urlRouterProvider.otherwise('/');
 
