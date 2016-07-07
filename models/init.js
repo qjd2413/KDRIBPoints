50840eba96c14b5dc4e85cca97628babef3333be
Quinn Davis <qjd2413@g.rit.edu>
2016-07-07 04:08:59 -0400

    Utilize lodash to condense init

--- models/init.js
+++ models/init.js
@@ -2,40 +2,35 @@ var positions = require('./data/positions.json');
 var auths = require('./data/authorizations.json');
 
 var q = require('q');
+var lodash = require('lodash');
 
 module.exports = function(db) { 
-
-    var auth_calls = [];
-    for(var i = 0; i < auths.length; i++) {
-      auth_calls.push(db.Authorization.findOrCreate({
-        where: auths[i],
-        defaults: auths[i]
+    q.all(lodash.map(auths, function(auth) {
+      return db.Authorization.findOrCreate({
+        where: auth,
+        defaults: auth
+      });
+    }))
+    .then(function(data) {
+      var auth = {};
+      lodash.map(data, function(curr) {
+        var tmp = curr[0].dataValues;
+        auth[tmp.level] = tmp.id;
+      })
+      return q.all(lodash.map(positions, function(position) {
+        position.AuthorizationId = auth[positions.AuthLevel];
+        delete position.AuthLevel;
+        return db.Position.findOrCreate({
+          where: position
+        });
       }));
-    }
-    q.all(auth_calls)
-      .then(function(data) {
-        var auth = {};
-        var values;
-        for(var i = 0; i < data.length; i++) {
-          values = data[i][0].dataValues;
-          auth[values.level] = values.id;
-        }
-        var position_calls = [];
-        for(var i = 0; i < positions.length; i++) {
-          positions[i].AuthorizationId = auth[positions[i].AuthLevel];
-          delete positions[i].AuthLevel;
-          position_calls.push(db.Position.findOrCreate({
-            where: positions[i]
-          }));
-        }
-        return q.all(position_calls);
 
-      })
-      .then(function() {
-        console.log('Database initialized.');
-      })
-      .fail(function(err) {
-        console.log('Error initializing database');
-        console.log(err);
-      })
+    })
+    .then(function() {
+      console.log('Database initialized.');
+    })
+    .fail(function(err) {
+      console.log('Error initializing database');
+      console.log(err);
+    })
 };
