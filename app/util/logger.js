3d563d2cf8954fbc65ee8af617892998ede39ef8
Quinn Davis <qjd2413@g.rit.edu>
2016-07-08 01:44:34 -0400

    Print date to log file

--- app/util/logger.js
+++ app/util/logger.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var dateFormat = require('dateformat');
+var lodash = require('lodash');
 
 var config = require('../../config/config.js').logger;
 
@@ -14,7 +15,30 @@ var log = function(label, args) {
       console.log(string);
     }
     if(config.file) {
-      fs.appendFileSync(config.file, string+'\n');
+      fs.stat(config.file, function(err, stats) {
+        var daystamp = false;
+        var today = new Date().toLocaleDateString();
+        if(err) {
+          //file doesn't exist
+          daystamp = true;
+        } else {
+          var content = fs.readFileSync(config.file, 'utf8');
+          content = lodash.without(content.split('\n'), '');
+          var i, lastDate;
+          for(i = content.length-1; i >= 0; i--) {
+            if(content[i].match(/\d{1,2}\/\d{1,2}\/\d{1,4}/)) {
+              lastDate = Date.parse(content[i]);
+              break;              
+            } 
+          }
+          todayDate = Date.parse(today);
+          daystamp = todayDate > lastDate;
+        }
+        if(daystamp) {
+          string = today + '\n' + string;
+        }
+        fs.appendFileSync(config.file, string+'\n');
+      });
     }
 }
 
