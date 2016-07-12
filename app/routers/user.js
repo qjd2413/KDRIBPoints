fa4f9470bdca49825897945b6c6e1ce97289a023
Quinn Davis <qjd2413@g.rit.edu>
2016-07-11 23:59:01 -0400

    Change console.log to relevant logger functions

--- app/routers/user.js
+++ app/routers/user.js
@@ -4,6 +4,7 @@
     var express = require('express');
     var router = express.Router();
     var config = require('../../config/google_config');
+    var logger = require('../util/logger.js');
     var db = require('../controllers/db.js');
 
     var passport = require('passport');
@@ -17,7 +18,7 @@
             callbackURL: config.callback
         },
         function(accessToken, refreshToken, profile, cb) {
-            console.log(profile.displayName,'has signed in.');
+            logger.info(profile.displayName,'has signed in.');
             db.sign_in(profile.id, profile.name, profile. emails);
             return cb(null, profile.id);
         })
@@ -39,7 +40,7 @@
             passport.authenticate('google', 
               { scope: ['profile', 'email'], hd: 'kdrib.org' })(req,res)
           } else {
-            console.log('User', req.user, 'already logged in.');
+            logger.warn('User', req.user, 'already logged in.');
             res.redirect('/'); 
           }
         });
@@ -53,7 +54,7 @@
 
         router.get('/sign_out', function(req, res) {
           if(req.user) {
-            console.log('User', req.user, ' has logged out.');
+            logger.info('User', req.user, ' has logged out.');
             req.logout();
           }
           res.redirect('/');
